65e719360b8d0f913d71779b072c5aeaf3aba564
Snowplou <snowplou01@gmail.com>
2023-02-16 18:50:44 -0800

    started making category

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
     import Buttons from "./lib/Buttons.svelte";
+    import Categories from "./lib/Categories.svelte";
     import Editor from "./lib/Editor.svelte";
     import Info from "./lib/Info.svelte";
     export let teamSelected = ""
+    export let showCategories = false
   
 </script>
 
 <main>
 
-  <Buttons />
+  <Buttons bind:showCategories/>
   <Info bind:teamSelected/>
   <Editor bind:teamSelected/>
+  <Categories bind:showCategories/>
   
 </main>
 
---
+++ src/lib/Categories.svelte
@@ -0,0 +1,70 @@
+<script>
+    import { categories, updateDb } from "../database";
+    export let showCategories = false
+</script>
+
+{#if showCategories}
+    <div id="flexCategory">
+        <div id="topCategory">
+            <p id="titleCategory"><b>Category Editor</b></p>
+            <img
+                src="redX.png"
+                alt="Close Editor"
+                on:click={() => (showCategories = false)}
+                on:keypress={() => (showCategories = false)}
+            />
+        </div>
+
+        {#key $categories}
+        {#if $categories}
+        {#each Object.keys($categories) as category}
+            <div class="edit">
+                {category}
+                <select value={$categories[category].type}>
+                    <option value="Number">Number</option>
+                    <option value="String">String</option>
+                    <option value="Boolean">Boolean</option>
+                    <option value="Dropdown">Dropdown</option>
+                </select>
+            </div>
+        {/each}
+        {/if}
+        {/key}
+    </div>
+{/if}
+
+
+<style>
+    .edit {
+        margin: 2%;
+        background-color: rgb(84, 121, 215);
+        font-size: 125%;
+    }
+
+    img {
+        margin: 2%;
+        cursor: pointer;
+        width: 15%;
+    }
+
+    #titleCategory {
+        margin-left: 28%;
+    }
+
+    #topCategory {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #flexCategory {
+        position: absolute;
+        left: 0%;
+        top: 0%;
+        transform: translate(10%, 5%);
+        background-color: gray;
+        width: 80vw;
+        height: 85vh;
+        opacity: 90%;
+        border-radius: 10px;
+    }
+</style>

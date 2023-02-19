e0250befebbf01db29a98b5d7412bee4865e7547
Snowplou <snowplou01@gmail.com>
2023-02-19 11:38:29 -0800

    made login screen

--- src/App.svelte
+++ src/App.svelte
@@ -5,21 +5,28 @@
     import Filters from "./lib/Filters.svelte";
     import Info from "./lib/Info.svelte";
     import Options from "./lib/Options.svelte"
+    import SignIn from "./lib/SignIn.svelte";
     export let teamSelected = ""
     export let showCategories = false
     export let selectedOption = ""
     export let selectedFilter = ""
+    export let team = ""
+    export let event = ""
   
 </script>
 
 <main>
 
+  {#if team != ""}
   <Buttons bind:showCategories bind:teamSelected bind:selectedOption bind:selectedFilter/>
   <Info bind:showCategories bind:teamSelected bind:selectedOption bind:selectedFilter/>
   <Editor bind:teamSelected/>
   <Categories bind:showCategories bind:selectedOption bind:selectedFilter/>
   <Options bind:selectedOption/>
   <Filters bind:selectedFilter/>
+  {:else}
+  <SignIn bind:team/>
+  {/if}
   
 </main>
 
---
+++ src/lib/SignIn.svelte
@@ -0,0 +1,80 @@
+<script>
+    export let team = "";
+</script>
+
+<p id="signInTitle">Enter Your Team Number And Password</p>
+<div id="signIn">
+    <div class="section">
+        <p>Team:</p>
+        <input type="text" />
+    </div>
+    <div class="section">
+        <p>Password:</p>
+        <input type="password" />
+    </div>
+    <button>Sign In</button>
+    <button>Sign Up</button>
+</div>
+
+<style>
+    #signInTitle {
+        text-align: center;
+        font-size: 200%;
+    }
+
+    input {
+        text-align: center;
+    }
+
+    button {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 5vh;
+        width: 50vw;
+        height: 8vh;
+        border-radius: 1000px;
+        border: none;
+        cursor: pointer;
+        background-color: rgb(84, 121, 215);
+        color: white;
+        font-size: 30px;
+        font-family: "Outfit", sans-serif;
+        font-weight: 400;
+    }
+
+    #signIn p {
+        font-size: 150%;
+        margin-right: 5vw;
+    }
+
+    .section {
+        display: flex;
+        align-content: center;
+        justify-content: space-around;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: baseline;
+        color: white;
+    }
+
+    input {
+        height: 5vh;
+        width: 35vw;
+    }
+
+    #signIn {
+        display: flex;
+        margin-left: auto;
+        margin-right: auto;
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: center;
+        align-items: baseline;
+        align-content: center;
+        background-color: gray;
+        width: 80vw;
+        height: 70vh;
+        border-radius: 10px;
+    }
+</style>

cc29daf5aeeebd192aec9048ec91ffd2a4aab7f8
Snowplou <snowplou01@gmail.com>
2024-01-28 17:13:46 -0800

    made notes resizeable, fixed image having cursor pointer

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -77,7 +77,7 @@
                     {#if info != "Notes"}
                     <input type="text" value={value(info)} on:change={(element) => update(info, element)} style="width: 80%; margin-left: 1%;"/>
                     {:else}
-                    <textarea value={value(info)} on:change={(element) => update(info, element)} style="width: 80%; margin-left: 1%; resize: none; height: 8vh;"></textarea>
+                    <textarea value={value(info)} on:change={(element) => update(info, element)} style="width: 95%; margin-left: 1%; resize: vertical; min-height: 8vh;"></textarea>
                     {/if}
                 {/if}
                 {#if type(info) == "Number"}
@@ -126,7 +126,6 @@
 
     img {
         margin: 2%;
-        cursor: pointer;
         width: 5vh;
     }
 
